0057f800a1e62935bb3b16ef3a2e4834f2e70e3e
Ezequiel <e.cf@leiuq.fun>
2025-12-16 17:30:47 +0100

    made a store for todos so it's easy for me manage the todos

---
+++ src/lib/todos.ts
@@ -0,0 +1,23 @@
+import { writable } from "svelte/store";
+
+export type Todo = {
+    id: number;
+    task: string;
+    completed: boolean;
+    onFocus: boolean;
+    priority: number;
+}
+
+export function createTodosStore() {
+    const { subscribe, set, update } = writable<Todo[]>([]);
+
+    return {
+        subscribe,
+        addTodo: (todo: Todo) => update(t => ([...t, todo])),
+        removeTodo: (id: number) => update(t => t.filter(todo => todo.id !== id)),
+        toggleComplete: (id: number) => update(t => t.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo)),
+        setFocus: (id: number, focus: boolean) => update(t => t.map(todo => todo.id === id ? { ...todo, onFocus: focus } : todo)),
+        setPriority: (id: number, priority: number) => update(t => t.map(todo => todo.id === id ? { ...todo, priority } : todo)),
+        clear: () => set([])
+    }
+}
